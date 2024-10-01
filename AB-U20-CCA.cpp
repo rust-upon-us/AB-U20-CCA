ed045312bf9d333ca9a69595d7f77b7b665f5673
Christian <chrstian.cardo.dev@gmail.com>
2024-10-01 20:30:47 +0200

    Agregar archivos de proyecto.

---
+++ AB-U20-CCA.cpp
@@ -0,0 +1,12 @@
+﻿// AB-U20-CCA.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "AB-U20-CCA.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
